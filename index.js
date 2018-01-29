a86601a11e5b383560b08cb8668cce361d0b62eb
David Clark <dlc.clark@gmail.com>
2018-01-29 21:01:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ function moveDodgerRight() {
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
   window.requestAnimationFrame(function(){
-    const right = positionToInteger(dodger.style.left);
+    const right = positionToInteger(dodger.style.left) + 40;
     
     if (left > 360){ //width of game, less size of dodger
       dodger.style.left = `${left + 4}px`
