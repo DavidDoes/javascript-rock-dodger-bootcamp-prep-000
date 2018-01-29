f3e12849d5e38dfe452ddb8cce89265add5c979b
David Clark <dlc.clark@gmail.com>
2018-01-29 20:51:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -165,7 +165,11 @@ function moveDodgerRight() {
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
   window.requestAnimationFrame(function(){
+    const left = positionToInteger(dodger.style.left);
     
+    if (left > 360){ //width of game, less size of dodger
+      dodger.style.left = `${left + 4}px`
+    }
   })
 }
 
