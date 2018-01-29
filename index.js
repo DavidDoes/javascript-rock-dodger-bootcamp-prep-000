f92ed06c8dad37dad702ee5d551bf32bc6ba5d69
David Clark <dlc.clark@gmail.com>
2018-01-29 20:25:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -119,6 +119,7 @@ function createRock(x) {
 function endGame() {
   clearInterval(gameInterval);
   ROCKS.forEach(function(rock) { rock.remove() });
+  document.removeEventListener('keydown', moveDodger);
   
 }
 
